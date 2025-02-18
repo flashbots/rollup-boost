b527e63e7f48f81c737294482adde6050a85f6b4
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-02-18 12:09:21 +0000

    Add partial block building

--- src/flashblocks/mod.rs
+++ src/flashblocks/mod.rs
@@ -1,4 +1,28 @@
+mod inbound;
 mod primitives;
 mod service;
+use inbound::FlashblocksReceiverService;
+use tokio::sync::mpsc;
 
 pub use service::FlashblocksService;
+
+pub struct Flashblocks {}
+
+impl Flashblocks {
+    pub fn run(builder_url: String) -> eyre::Result<FlashblocksService> {
+        let (tx, rx) = mpsc::channel(100);
+        let receiver = FlashblocksReceiverService::new(builder_url, tx).unwrap();
+
+        tokio::spawn(async move {
+            let _ = receiver.run().await;
+        });
+
+        let service = FlashblocksService::new();
+        let mut service_handle = service.clone();
+        tokio::spawn(async move {
+            service_handle.run(rx).await;
+        });
+
+        Ok(service)
+    }
+}
