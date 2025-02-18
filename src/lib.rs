9c2632311e3f3eaaaf94addaba5f42c23ef24c45
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-02-18 21:49:47 +0000

    Fix imports and make fb service public

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ pub mod flashblocks;
 pub use flashblocks::primitives::{
     ExecutionPayloadBaseV1, ExecutionPayloadFlashblockDeltaV1, FlashblocksPayloadV1,
 };
+
+pub use flashblocks::Flashblocks;
