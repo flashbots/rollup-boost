ecb42cf9200b0f408146dba0a2295c6e2976123d
Eric Woolsey <ewoolsey@ualberta.ca>
2025-04-04 12:03:59 -0700

    improve forward_miner_set_gas_price  test reliability

--- src/proxy.rs
+++ src/proxy.rs
@@ -639,7 +639,6 @@ mod tests {
 
     #[tokio::test]
     async fn test_forward_miner_set_gas_price() -> eyre::Result<()> {
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         let test_harness = TestHarness::new().await?;
 
         let gas_price = U128::ZERO;
@@ -651,6 +650,7 @@ mod tests {
             .await?;
 
         let expected_price = json!(gas_price);
+        tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
 
         // Assert the builder received the correct payload
         let builder = &test_harness.builder;
