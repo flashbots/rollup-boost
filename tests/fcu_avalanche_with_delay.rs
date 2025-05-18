21f11002f5cfd873cea1c2bbcf281b8a96d17fb1
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-05-18 21:09:44 +0100

    Add test to ensure the builder can still produce blocks with short fcu spans

---
+++ tests/fcu_avalanche_with_delay.rs
@@ -0,0 +1,60 @@
+mod common;
+
+use common::RollupBoostTestHarnessBuilder;
+use common::proxy::ProxyHandler;
+use futures::FutureExt;
+use serde_json::Value;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::time::Duration;
+
+// Create a dynamic handler that delays all the calls by 2 seconds
+struct DelayHandler {
+    delay: Duration,
+}
+
+impl DelayHandler {
+    pub fn new(delay: Duration) -> Self {
+        Self { delay }
+    }
+}
+
+impl ProxyHandler for DelayHandler {
+    fn handle(
+        &self,
+        _method: String,
+        _params: Value,
+        _result: Value,
+    ) -> Pin<Box<dyn Future<Output = Option<Value>> + Send>> {
+        let delay = self.delay;
+        async move {
+            tokio::time::sleep(delay).await;
+            None
+        }
+        .boxed()
+    }
+}
+
+#[tokio::test]
+async fn fcu_avalanche_with_delay() -> eyre::Result<()> {
+    // This test ensures that even with delay in between RB and the external builder (50ms) the
+    // builder can still build the block
+    let delay = DelayHandler::new(Duration::from_millis(50));
+
+    let harness = RollupBoostTestHarnessBuilder::new("fcu_avalanche_with_delay")
+        .proxy_handler(Arc::new(delay))
+        .build()
+        .await?;
+    let mut block_generator = harness.block_generator().await?;
+    block_generator.set_block_time(Duration::from_millis(0));
+
+    for _ in 0..30 {
+        let (_block, block_creator) = block_generator.generate_block(false).await?;
+        assert!(
+            block_creator.is_builder(),
+            "Block creator should be the builder"
+        );
+    }
+
+    Ok(())
+}
