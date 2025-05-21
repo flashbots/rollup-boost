e2fcd4ad787a6263d202226ff377c4574c1f9c93
Eric Woolsey <ewoolsey@ualberta.ca>
2025-05-21 09:16:55 -0700

    switch to join handle abort

--- src/consistent_request.rs
+++ src/consistent_request.rs
@@ -3,14 +3,14 @@ use std::{
     time::Duration,
 };
 
-use eyre::bail;
+use eyre::{bail, eyre};
 use http::{request, response};
 use jsonrpsee::{
     core::{BoxError, http_helpers},
     http_client::{HttpBody, HttpRequest, HttpResponse},
 };
 use parking_lot::Mutex;
-use tokio::sync::watch;
+use tokio::{sync::watch, task::JoinHandle};
 use tracing::{error, info, warn};
 
 use crate::{ExecutionMode, Health, HttpClient, Probes};
@@ -56,74 +56,102 @@ impl ConsistentRequest {
             execution_mode,
         };
 
-        let mut clone = manager.clone();
+        let clone = manager.clone();
         tokio::spawn(async move {
-            let mut failed = false;
+            let mut attempt: Option<JoinHandle<_>> = None;
             loop {
-                tokio::select! {
-                _ = req_rx.changed() => {
-                    let (parts, body) = {
-                        req_rx.borrow_and_update()
-                            .as_ref()
-                            .expect("value should always be Some")
-                            .clone()
-                    };
-                    failed = clone.try_send(parts.clone(), body.clone(), res_tx.clone()).await.is_err();
-                }
-                _ = async {
-                        if failed {
-                            tokio::time::sleep(Duration::from_millis(200)).await
-                        } else {
-                            std::future::pending::<()>().await
-                        }
-                    } => {
-                        let (parts, body) = {
-                            req_rx.borrow()
-                                .as_ref()
-                                .expect("value should always be Some")
-                                .clone()
-                        };
-                        failed = clone.send_to_builder(parts.clone(), body.clone()).await.is_err();
+                req_rx
+                    .changed()
+                    .await
+                    .expect("channel should always be open");
+
+                if let Some(attempt) = attempt {
+                    if !attempt.is_finished() {
+                        error!(target: "proxy::call", method = clone.method, message = "request cancelled");
+                        attempt.abort();
                     }
                 }
+
+                let (parts, body) = req_rx
+                    .borrow_and_update()
+                    .as_ref()
+                    .expect("value should always be Some")
+                    .clone();
+
+                let mut clone = clone.clone();
+                let res_tx_clone = res_tx.clone();
+                attempt = Some(tokio::spawn(async move {
+                    clone
+                        .send_with_retry_cancel_safe(parts, body, res_tx_clone)
+                        .await
+                }));
             }
         });
 
         manager
     }
 
-    async fn try_send(
+    /// This function may be cancelled at any time from an incoming request.
+    /// We should ensure we're in a valid state at all await points.
+    async fn send_with_retry_cancel_safe(
         &mut self,
         parts: request::Parts,
         body: Vec<u8>,
-        res_sender: watch::Sender<Option<Result<(response::Parts, Vec<u8>), BoxError>>>,
+        res_tx: watch::Sender<Option<Result<(response::Parts, Vec<u8>), BoxError>>>,
     ) -> eyre::Result<()> {
         // We send the l2 request first, because we need to avoid the situation where the
         // l2 fails and the builder succeeds. If this were to happen, it would bee too dangerous to
         // return the l2 error response back to the caller, since the builder would now have an
-        // invalid state.
+        // invalid state. We can return early if the l2 request fails.
+        let mut manager = self.clone();
+        let parts_clone = parts.clone();
+        let body_clone = body.clone();
+        let res_sender_clone = res_tx.clone();
+        tokio::spawn(async move {
+            manager
+                .send_to_l2(parts_clone, body_clone, res_sender_clone)
+                .await
+        })
+        .await??;
+
+        loop {
+            let mut manager = self.clone();
+            let parts_clone = parts.clone();
+            let body_clone = body.clone();
+            match tokio::spawn(
+                async move { manager.send_to_builder(parts_clone, body_clone).await },
+            )
+            .await?
+            {
+                Ok(_) => return Ok(()),
+                Err(_) => {
+                    tokio::time::sleep(Duration::from_millis(200)).await;
+                }
+            }
+        }
+    }
+
+    async fn send_to_l2(
+        &mut self,
+        parts: request::Parts,
+        body: Vec<u8>,
+        res_tx: watch::Sender<Option<Result<(response::Parts, Vec<u8>), BoxError>>>,
+    ) -> eyre::Result<()> {
         let l2_req = HttpRequest::from_parts(parts.clone(), HttpBody::from(body.clone()));
         let l2_res = self.l2_client.forward(l2_req, self.method.clone()).await;
 
         // Return the l2 response asap
-        let l2_res_parts = match l2_res {
+        let (res, l2_res_parts) = match l2_res {
             Ok(t) => {
                 let (parts, body) = t.into_parts();
                 let (body_bytes, _) =
                     http_helpers::read_body(&parts.headers, body, u32::MAX).await?;
-                Ok((parts, body_bytes))
+                (Ok(()), Ok((parts, body_bytes)))
             }
-            Err(e) => Err(e),
+            Err(e) => (Err(eyre!("failed to send request to l2: {e}")), Err(e)),
         };
-        let l2_res_is_ok = l2_res_parts.is_ok();
-        res_sender.send(Some(l2_res_parts))?;
-
-        if !l2_res_is_ok {
-            // If the l2 request failed, we don't need to send the builder request
-            return Ok(());
-        }
-
-        self.send_to_builder(parts, body).await
+        res_tx.send(Some(l2_res_parts))?;
+        res
     }
 
     async fn send_to_builder(&mut self, parts: request::Parts, body: Vec<u8>) -> eyre::Result<()> {
