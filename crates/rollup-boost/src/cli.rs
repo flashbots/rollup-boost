081e892354a35b585c4d39bb7902ac55b0acf1a8
0xKitsune <0xkitsune@protonmail.com>
2025-06-29 00:03:22 -0400

    feat: spawn ping task

--- crates/rollup-boost/src/cli.rs
+++ crates/rollup-boost/src/cli.rs
@@ -13,8 +13,7 @@ use tokio::signal::unix::{SignalKind, signal as unix_signal};
 use tracing::{Level, info};
 
 use crate::{
-    BlockSelectionPolicy, DebugClient, FlashblocksArgs, FlashblocksManager, ProxyLayer,
-    RollupBoostServer, RpcClient,
+    BlockSelectionPolicy, DebugClient, FlashblocksArgs, ProxyLayer, RollupBoostServer, RpcClient,
     client::rpc::{BuilderArgs, L2ClientArgs},
     debug_api::ExecutionMode,
     get_version, init_metrics,
--- crates/rollup-boost/src/flashblocks/manager.rs
+++ crates/rollup-boost/src/flashblocks/manager.rs
@@ -1,12 +1,23 @@
 use crate::flashblocks::inbound::FlashblocksReceiverService;
 use crate::{FlashblocksService, RpcClient};
 use core::net::SocketAddr;
+use futures::stream::SplitSink;
+use futures::{SinkExt, StreamExt};
+use std::time::Duration;
+use tokio::net::TcpStream;
 use tokio::sync::mpsc::{self, Sender};
+use tokio::task::JoinHandle;
+use tokio_tungstenite::tungstenite::{self, Message};
+use tokio_tungstenite::{WebSocketStream, connect_async};
+use tokio_util::bytes::Bytes;
+use tokio_util::sync::CancellationToken;
 use url::Url;
 
 use super::FlashblocksPayloadV1;
 
-pub struct FlashblocksManager;
+pub struct FlashblocksManager {
+    builder_ws_endpoint: Url,
+}
 
 impl FlashblocksManager {
     pub fn new() -> Self {
@@ -14,17 +25,35 @@ impl FlashblocksManager {
     }
 
     pub fn spawn(self) {
-        todo!();
-
         let (tx, rx) = mpsc::channel(100);
         self.subscribe_flashblocks(tx);
-        // TODO: establish stream with builder
 
         // TODO: serve stream to multiple connections
+
+        todo!();
     }
 
-    pub fn subscribe_flashblocks(&self, tx: Sender<FlashblocksPayloadV1>) {
+    pub async fn subscribe_flashblocks(
+        &self,
+        tx: Sender<FlashblocksPayloadV1>,
+    ) -> Result<(), FlashblocksManagerError> {
         // TODO: establish connection
+        let (ws_stream, _) = connect_async(self.builder_ws_endpoint.as_str()).await?;
+        let (mut sink, mut stream) = ws_stream.split();
+
+        // TODO: spawn_ping
+        let ping_handle: JoinHandle<Result<(), FlashblocksManagerError>> =
+            tokio::spawn(async move {
+                let mut ping_interval = tokio::time::interval(Duration::from_millis(500));
+                loop {
+                    ping_interval.tick().await;
+                    sink.send(Message::Ping(Bytes::new()))
+                        .await
+                        .map_err(|_| FlashblocksManagerError::PingFailed)?;
+                }
+            });
+
+        Ok(())
     }
 
     pub fn run(
@@ -49,4 +78,23 @@ impl FlashblocksManager {
 
         Ok(service)
     }
+}
+
+// fn spawn_ping<S, Item>(mut sink: SplitSink<WebSocketStream<S>, Item>) -> JoinHandle<()> {
+//     tokio::spawn(async move {
+//         let mut ping_interval = tokio::time::interval(Duration::from_millis(500));
+//         loop {
+//             ping_interval.tick().await;
+//             sink.send(Message::Ping(Bytes::new()));
+//         }
+//     })
+// }
+//
+
+#[derive(thiserror::Error, Debug)]
+pub enum FlashblocksManagerError {
+    #[error("Ping failed")]
+    PingFailed,
+    #[error(transparent)]
+    ConnectError(#[from] tungstenite::Error),
 }
