c7130aa6cf7e0ba8587f8058843bd8b213e1c64f
Eric Woolsey <ewoolsey@ualberta.ca>
2025-08-15 15:06:04 -0700

    remove websocket config

--- crates/rollup-boost/src/cli.rs
+++ crates/rollup-boost/src/cli.rs
@@ -3,18 +3,13 @@ use clap::Parser;
 use eyre::bail;
 use jsonrpsee::{RpcModule, server::Server};
 use parking_lot::Mutex;
-use std::{
-    net::{IpAddr, SocketAddr},
-    path::PathBuf,
-    str::FromStr,
-    sync::Arc,
-};
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use tokio::signal::unix::{SignalKind, signal as unix_signal};
 use tracing::{Level, info};
 
 use crate::{
-    BlockSelectionPolicy, Flashblocks, FlashblocksArgs, FlashblocksKeys, ProxyLayer,
-    RollupBoostServer, RpcClient,
+    BlockSelectionPolicy, FlashblocksArgs, FlashblocksKeys, ProxyLayer, RollupBoostServer,
+    RpcClient,
     client::rpc::{BuilderArgs, L2ClientArgs},
     debug_api::ExecutionMode,
     get_version, init_metrics,
@@ -147,51 +142,22 @@ impl RollupBoostArgs {
         let (probe_layer, probes) = ProbeLayer::new();
         let execution_mode = Arc::new(Mutex::new(self.execution_mode));
 
-        let (rpc_module, health_handle): (RpcModule<()>, _) =
-            if let Some(flashblocks_args) = self.flashblocks {
-                let inbound_url = flashblocks_args.flashblocks_builder_url;
-                let outbound_addr = SocketAddr::new(
-                    IpAddr::from_str(&flashblocks_args.flashblocks_host)?,
-                    flashblocks_args.flashblocks_port,
-                );
+        let (rpc_module, health_handle): (RpcModule<()>, _) = {
+            let rollup_boost = RollupBoostServer::new(
+                l2_client,
+                Arc::new(builder_client),
+                execution_mode.clone(),
+                self.block_selection_policy,
+                probes.clone(),
+            );
 
-                let builder_client = Arc::new(Flashblocks::run(
-                    builder_client.clone(),
-                    inbound_url,
-                    outbound_addr,
-                    flashblocks_args.flashblock_builder_ws_reconnect_ms,
-                )?);
+            let health_handle = rollup_boost
+                .spawn_health_check(self.health_check_interval, self.max_unsafe_interval);
 
-                let rollup_boost = RollupBoostServer::new(
-                    l2_client,
-                    builder_client,
-                    execution_mode.clone(),
-                    self.block_selection_policy,
-                    probes.clone(),
-                );
-
-                let health_handle = rollup_boost
-                    .spawn_health_check(self.health_check_interval, self.max_unsafe_interval);
-
-                // Spawn the debug server
-                rollup_boost.start_debug_server(debug_addr.as_str()).await?;
-                (rollup_boost.try_into()?, health_handle)
-            } else {
-                let rollup_boost = RollupBoostServer::new(
-                    l2_client,
-                    Arc::new(builder_client),
-                    execution_mode.clone(),
-                    self.block_selection_policy,
-                    probes.clone(),
-                );
-
-                let health_handle = rollup_boost
-                    .spawn_health_check(self.health_check_interval, self.max_unsafe_interval);
-
-                // Spawn the debug server
-                rollup_boost.start_debug_server(debug_addr.as_str()).await?;
-                (rollup_boost.try_into()?, health_handle)
-            };
+            // Spawn the debug server
+            rollup_boost.start_debug_server(debug_addr.as_str()).await?;
+            (rollup_boost.try_into()?, health_handle)
+        };
 
         // Build and start the server
         info!("Starting server on :{}", self.rpc_port);
@@ -324,13 +290,6 @@ pub mod tests {
             .flashblocks
             .expect("flashblocks should be Some when flag is passed");
         assert!(flashblocks.flashblocks);
-        assert_eq!(
-            flashblocks.flashblocks_builder_url.to_string(),
-            "ws://127.0.0.1:1111/"
-        );
-        assert_eq!(flashblocks.flashblocks_host, "127.0.0.1");
-        assert_eq!(flashblocks.flashblocks_port, 1112);
-        assert_eq!(flashblocks.flashblock_builder_ws_reconnect_ms, 5000);
 
         Ok(())
     }
@@ -348,27 +307,12 @@ pub mod tests {
             FLASHBLOCKS_SK,
             "--flashblocks-builder-vk",
             FLASHBLOCKS_VK,
-            "--flashblocks-builder-url",
-            "ws://example.com:9999",
-            "--flashblocks-host",
-            "0.0.0.0",
-            "--flashblocks-port",
-            "2222",
-            "--flashblock-builder-ws-reconnect-ms",
-            "10000",
         ])?;
 
         let flashblocks = args
             .flashblocks
             .expect("flashblocks should be Some when flag is passed");
         assert!(flashblocks.flashblocks);
-        assert_eq!(
-            flashblocks.flashblocks_builder_url.to_string(),
-            "ws://example.com:9999/"
-        );
-        assert_eq!(flashblocks.flashblocks_host, "0.0.0.0");
-        assert_eq!(flashblocks.flashblocks_port, 2222);
-        assert_eq!(flashblocks.flashblock_builder_ws_reconnect_ms, 10000);
 
         Ok(())
     }
--- crates/rollup-boost/src/flashblocks/args.rs
+++ crates/rollup-boost/src/flashblocks/args.rs
@@ -1,6 +1,5 @@
 use clap::Args;
 use ed25519_dalek::{SigningKey, VerifyingKey};
-use url::Url;
 
 use hex::FromHex;
 
@@ -10,22 +9,6 @@ pub struct FlashblocksArgs {
     /// Enable Flashblocks client
     #[arg(long, env, required = false)]
     pub flashblocks: bool,
-
-    /// Flashblocks Builder WebSocket URL
-    #[arg(long, env, default_value = "ws://127.0.0.1:1111")]
-    pub flashblocks_builder_url: Url,
-
-    /// Flashblocks WebSocket host for outbound connections
-    #[arg(long, env, default_value = "127.0.0.1")]
-    pub flashblocks_host: String,
-
-    /// Flashblocks WebSocket port for outbound connections
-    #[arg(long, env, default_value = "1112")]
-    pub flashblocks_port: u16,
-
-    /// Time used for timeout if builder disconnected
-    #[arg(long, env, default_value = "5000")]
-    pub flashblock_builder_ws_reconnect_ms: u64,
 
     #[arg(
         long,
