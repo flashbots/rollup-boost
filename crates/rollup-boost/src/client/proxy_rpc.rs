f66760e67ca366c4a67c7f245eec2aec0bbd86eb
Solar Mithril <mikawamp@gmail.com>
2025-09-16 22:00:54 +0500

    Better impl with hypers

--- crates/rollup-boost/src/client/proxy_rpc.rs
+++ crates/rollup-boost/src/client/proxy_rpc.rs
@@ -1,14 +1,21 @@
 use std::time::Duration;
 
 use crate::payload::PayloadSource;
-use crate::secret_to_bearer_header;
+use crate::{secret_to_bearer_header, AuthLayer};
 use alloy_json_rpc::{RpcError, RpcRecv, RpcSend};
 use alloy_rpc_client::RpcClient;
 use alloy_rpc_types_engine::JwtSecret;
 use alloy_transport::TransportResult;
+use alloy_transport_http::{Http, HyperClient};
 use http::header::AUTHORIZATION;
 use http::{HeaderMap, Uri};
+use http_body_util::Full;
+use hyper_util::client::legacy::Client;
+use hyper_util::rt::TokioExecutor;
 use opentelemetry::trace::SpanKind;
+use tower::ServiceBuilder;
+use tower::timeout::TimeoutLayer;
+use tower_http::decompression::DecompressionLayer;
 use tracing::{debug, error, instrument};
 
 #[derive(Clone, Debug)]
@@ -20,23 +27,26 @@ pub struct RpcProxyClient {
 
 impl RpcProxyClient {
     pub fn new(url: Uri, secret: JwtSecret, target: PayloadSource, timeout: u64) -> Self {
-        let mut headers = HeaderMap::new();
-        let mut auth_header = secret_to_bearer_header(&secret);
-        auth_header.set_sensitive(true);
-        headers.insert(AUTHORIZATION, auth_header);
+        let connector = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .expect("no native root CA certificates found")
+            .https_or_http()
+            .enable_http1()
+            .enable_http2()
+            .build();
 
-        // Create http client that alloys is using inside
-        let http_client = reqwest::ClientBuilder::default()
-            .timeout(Duration::from_millis(timeout))
-            .default_headers(headers)
-            .deflate(true)
-            .brotli(true)
-            .zstd(true)
-            .gzip(true)
-            .build()
-            .expect("rpc client creation");
-        let client =
-            RpcClient::builder().http_with_client(http_client, url.to_string().parse().unwrap());
+        let client = Client::builder(TokioExecutor::new()).build::<_, Full<hyper::body::Bytes>>(connector);
+
+        let service = ServiceBuilder::new()
+            // .layer(TimeoutLayer::new(Duration::from_millis(timeout)))
+            // .layer(DecompressionLayer::new())
+            .layer(AuthLayer::new(secret))
+            .service(client);
+
+        let layer_transport = HyperClient::with_service(service);
+        let http = Http::with_client(layer_transport, url.to_string().parse().unwrap());
+
+        let client = RpcClient::new(http, true);
 
         Self {
             client,
