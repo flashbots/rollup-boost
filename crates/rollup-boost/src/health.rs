ea91d899488f4aa074266124050e42931e5675ef
Karan Kurbur <karankurbur@gmail.com>
2025-10-30 15:22:42 -0700

    fix

--- crates/rollup-boost/src/health.rs
+++ crates/rollup-boost/src/health.rs
@@ -50,10 +50,8 @@ impl HealthHandle {
 
             loop {
                 sleep_until(Instant::now() + self.health_check_interval).await;
-                info!("health check loop");
-
                 let t = timestamp.tick();
-
+                
                 // Check L2 client health. If its unhealthy, set the health status to ServiceUnavailable
                 // If in disabled or dry run execution mode, set the health status to Healthy if the l2 client is healthy
                 match self
